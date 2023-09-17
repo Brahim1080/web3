ee31c282a86dca3cad87a057645a20a35511728f
Brahim <brahim.abid@student.vinci.be>
2023-09-17 22:08:32 +0200

    1.1-1.2 : import image

--- module1/1.1-1.2/src/components/Header/Header.jsx
+++ module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
+import logo from './LOGO HE VINCI.png';
 const Header = ({ course }) => {
-    return (
-      <div>
-        <h1>{course}</h1>
-      </div>
-    );
-  };
-  
-  export default Header;
-  
+  return (
+    <div>
+      <img src={logo} alt="logo Vinci"></img>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+
+export default Header;
