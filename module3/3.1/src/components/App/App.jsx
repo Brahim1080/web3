f5bed3f0cad85554d0e9c1d2aa83137726096e92
Brahim <brahim.abid@student.vinci.be>
2023-10-28 11:31:38 +0200

    module3 : basics

--- module3/3.1/src/components/App/App.jsx
+++ module3/3.1/src/components/App/App.jsx
@@ -1,21 +1,6 @@
 import { useState } from "react";
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-);
-
-const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <p>
-        good {good} neutral {neutral} bad {bad} <br />
-        
-      </p>
-    </div>
-  );
-};
-
+import Button from "components/Buttons/Button";
+import Statistics from "components/Statistics/Statistics";
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -27,9 +12,10 @@ const App = () => {
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => setBad(bad +1)} text="bad" />
 
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Statistics</h1>
+      <Statistics {...{ good, neutral, bad }} />
     </div>
   );
 };
