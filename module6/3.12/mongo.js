3167a13c0d063be21c0462d37b97cfdcebe92eb7
Brahim <brahim.abid@student.vinci.be>
2023-11-01 13:46:23 +0100

    module6 : 3.12

---
+++ module6/3.12/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const password = process.env.DB_PASSWORD;
+
+const user = process.env.DB_USER;
+
+const url = `mongodb+srv://${user}:${password}@cluster0.igofgx6.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+// Define Schema and Model.
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// get all
+if (process.argv.length === 2) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => console.log(`${person.name} ${person.number}`));
+    mongoose.connection.close();
+  });
+  
+}
+// Insert one
+if (process.argv.length === 4) {
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3],
+  });
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
