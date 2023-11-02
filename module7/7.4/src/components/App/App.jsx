79f432ba7704b0ed2267cae48c621803d6c8866f
Brahim <brahim.abid@student.vinci.be>
2023-11-02 16:47:21 +0100

    module7 : 7.4

---
+++ module7/7.4/src/components/App/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { Route, Routes, useMatch, useNavigate } from "react-router-dom";
+import Notification from "../Notification/Notification";
+import CreateNew from "../Anecdotes/CreateNew";
+import Footer from "../Footer/Footer";
+import About from "../About/About";
+import AnecdoteList from "../Anecdotes/AnecdoteList";
+import Menu from "../Menu/Menu";
+import ViewOne from "../Anecdotes/ViewOne";
+import { Layout, message } from "antd";
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: 1,
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: 2,
+    },
+  ]);
+
+  const [notification, setNotification] = useState("");
+
+  const clearNotifcationWithDelay = () => {
+    setTimeout(() => setNotification(""), 5000);
+  };
+  useEffect(() => clearNotifcationWithDelay(), [notification]);
+
+  const navigate = useNavigate();
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000);
+    setAnecdotes(anecdotes.concat(anecdote));
+
+    setNotification(`new anecdote ${anecdote.content} created !!`);
+    navigate("/");
+  };
+
+  const match = useMatch("/anecdotes/:id");
+  const anecdote = match
+    ? anecdotes.find((a) => a.id === Number(match.params.id))
+    : null;
+  const { Header, Footer: AntdFooter, Content } = Layout;
+  return (
+    <>
+      <Header>
+        <h1 style={{ color: "white" }}>Software anecdotes</h1>
+      </Header>
+
+      <Content style={{ padding: "10px 50px" }}>
+        <Menu />
+
+        {notification && <Notification notification={notification} />}
+        <Routes>
+          <Route
+            path="/anecdotes/:id"
+            element={<ViewOne anecdote={anecdote} />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/create" element={<CreateNew addNew={addNew} />} />
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        </Routes>
+      </Content>
+      <AntdFooter>
+        <Footer />
+      </AntdFooter>
+    </>
+  );
+};
+
+export default App;
